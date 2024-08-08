67e872742bb124c53545334536f0f2c9fa3abc96
Rreyth <dhaussy.terry99@gmail.com>
2024-08-08 17:29:43 +0200

    setup file tree

---
+++ src/Swingy/utils/Print.java
@@ -0,0 +1,24 @@
+package Swingy.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+public class Print {
+	public static void print(Object p_message) {
+		System.out.println(p_message);
+	}
+
+	public static void printErr(Object p_message) {
+		System.err.println(p_message);
+	}
+
+	public static void printToFile(String p_message, String path) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+            writer.write(p_message);
+		}
+		catch (IOException e) {
+			printErr("Error writing to file: " + e.getMessage());
+		}
+	}
+}
